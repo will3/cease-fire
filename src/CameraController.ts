c938e7f1552e837067d40966ce5ecc35eac1e593
will <will@rushdigital.co.nz>
2019-11-08 15:44:44 +1300

    MINOR

--- src/CameraController.ts
+++ src/CameraController.ts
@@ -2,7 +2,7 @@ import { Camera, Euler, Vector3 } from "three";
 import Component from "./Component";
 
 export default class CameraController extends Component {
-    camera: Camera;
+    camera!: Camera;
     rotation = new Euler(-Math.PI * 0.3, Math.PI / 4, 0, 'YXZ');
     distance = 50;
     target = new Vector3();
